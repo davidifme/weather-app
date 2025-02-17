aed412d5d50686f7538ae5f7791fe514784ddab4
David <davidifme@icloud.com>
2025-02-17 19:42:01 +0100

    feat: Add weather GIFs and UI improvements
    - Added GIFs for different weather conditions
    (clouds, hot, rain, snow, sunny)
    - Updated `index.js` to include new stylesheets
    - Modified `template.html` to include settings and GIF container
    - Enhanced `weatherUI.js` to display appropriate GIF based on weather
    conditions
    - Created `gif.css` for styling the GIF container and preview content
    - Updated `main.css` for overall layout adjustments
    - Modified `searchbar.css` to remove form margins
    - Added `settings.css` for styling the settings section and buttons

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import "/styles/fonts.css";
 import "/styles/reset.css";
 import "/styles/main.css";
 import "/styles/searchbar.css";
+import "/styles/gif.css";
+import "/styles/settings.css";
 import { weatherManager } from "./weatherManager.js";
 
 weatherManager.init();
--- src/weatherUI.js
+++ src/weatherUI.js
@@ -1,4 +1,9 @@
 import { weatherManager } from "./weatherManager";
+import snowGif from "/gifs/snow.gif";
+import rainGif from "/gifs/rain.gif";
+import hotGif from "/gifs/hot.gif";
+import cloudsGif from "/gifs/clouds.gif";
+import sunnyGif from "/gifs/sunny.gif";
 
 export const weatherUI = (function() {
     const country = document.getElementById('country');
@@ -20,24 +25,44 @@ export const weatherUI = (function() {
     const sunrise = document.getElementById('sunrise');
     const sunset = document.getElementById('sunset');
 
+    const celsiusBtn = document.getElementById('celsius-button');
+    const fahrenheitBtn = document.getElementById('fahrenheit-button');
+
+    const gif = document.getElementById('gif');
+
+    let currentDegreeSetup = 'C';
+    let currentWeatherState;
+
     function initUI() {
         setupSearchBar();
+        setupButtons();
     }
 
     async function displayWeather() {
         const weatherData = await weatherManager.getWeatherData();
-        updateDisplayData(weatherManager.convertData(weatherData));
+        const convertedWeatherData = weatherManager.convertData(weatherData)
+
+        updateDisplayData(convertedWeatherData);
+        displayGif(convertedWeatherData);
     }
 
     async function updateDisplayData(weatherData) {
+        const convertTemp = (temp) => {
+            if (currentDegreeSetup === 'F') {
+                return `${(temp * 9/5 + 32).toFixed(1)}°F`;
+            } else if (currentDegreeSetup === 'C') {
+                return `${temp.toFixed(1)}°C`;
+            }
+        };
+
         conditions.textContent = weatherData.conditions;
         description.textContent = weatherData.description;
-        temp.textContent = `${weatherData.temp}°`;
-        feelslike.textContent = `${weatherData.feelslike}°`;
-        tempmax.textContent = `${weatherData.tempmax}°`;
-        tempmin.textContent = `${weatherData.tempmin}°`;
-        feelslikemax.textContent = `${weatherData.feelslikemax}°`;
-        feelslikemin.textContent = `${weatherData.feelslikemin}°`;
+        temp.textContent = convertTemp(weatherData.temp);
+        feelslike.textContent = convertTemp(weatherData.feelslike);
+        tempmax.textContent = `H:${convertTemp(weatherData.tempmax)}`;
+        tempmin.textContent = `L:${convertTemp(weatherData.tempmin)}`;
+        feelslikemax.textContent = `H:${convertTemp(weatherData.feelslikemax)}`;
+        feelslikemin.textContent = `L:${convertTemp(weatherData.feelslikemin)}`;
         humidity.textContent = `${weatherData.humidity}%`;
         windspeed.textContent = `${weatherData.windspeed} km/h`;
         pressure.textContent = `${weatherData.pressure} hPa`;
@@ -47,7 +72,7 @@ export const weatherUI = (function() {
         sunrise.textContent = weatherData.sunrise;
         sunset.textContent = weatherData.sunset;
 
-        const countryName = await weatherManager.getCountryName()
+        const countryName = await weatherManager.getCountryName();
         country.textContent = countryName.toLowerCase();
         location.textContent = await weatherManager.getCityName();
     }
@@ -66,6 +91,72 @@ export const weatherUI = (function() {
             await weatherManager.convertToCoordinates(searchBar.value);
             await displayWeather();
             searchBar.value = '';
+        });
+    }
+
+    function displayGif(weatherData) {
+        const temperature = weatherData.temp;
+        const humidity = weatherData.humidity;
+        const snow = weatherData.snow;
+        const cloudCover = weatherData.cloudcover;
+        
+        if (snow > 0) {
+            gif.src = snowGif;
+            currentWeatherState = 'snow';
+            return
+        }
+
+        if (humidity > 80) {
+            gif.src = rainGif;
+            currentWeatherState = 'rain';
+            return
+        }
+
+        if (cloudCover > 30) {
+            gif.src = cloudsGif;
+            currentWeatherState = 'clouds';
+            return
+        } else if (cloudCover < 20) {
+            gif.src = sunnyGif;
+            currentWeatherState = 'sunny';
+            return
+        }
+
+        if (temperature > 28) {
+            gif.src = hotGif;
+            currentWeatherState = 'hot';
+            return
+        }
+    }
+
+    function changeColors(weatherState) {
+
+    }
+
+    function setupButtons() {
+        if (currentDegreeSetup === 'F') {
+            celsiusBtn.disabled = false;
+            fahrenheitBtn.disabled = true;
+        } else if (currentDegreeSetup === 'C') {
+            celsiusBtn.disabled = true;
+            fahrenheitBtn.disabled = false;
+        } else {
+            console.error('Error: Unknown type of temperature degree.');
+            return;
+        }
+
+        celsiusBtn.addEventListener('click', async (e) => {
+            currentDegreeSetup = 'C';
+            celsiusBtn.disabled = true;
+            fahrenheitBtn.disabled = false;
+            await displayWeather();
+        });
+
+        fahrenheitBtn.addEventListener('click', async (e) => {
+            currentDegreeSetup = 'F';
+            celsiusBtn.disabled = false;
+            fahrenheitBtn.disabled = true;
+            await displayWeather();
         });
     }
 
